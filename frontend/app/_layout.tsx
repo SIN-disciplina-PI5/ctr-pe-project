26d9f9131292fa3842b29f4bb5053ec27cda04f5
setbr1 <dcdm2mil@gmail.com>
2026-06-06 16:57:20 -0300

    frontend fix: queryclient para a home

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,7 +1,14 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { Slot } from "expo-router";
 import React from "react";
+
+import { queryClient } from "@/infrastructure/api/query-client";
 import "../global.css";
 
 export default function RootLayout() {
-  return <Slot />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Slot />
+    </QueryClientProvider>
+  );
 }
