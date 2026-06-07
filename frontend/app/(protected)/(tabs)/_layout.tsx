5ddadf128ec07ef2c133a94053039f70553473ca
setbr1 <dcdm2mil@gmail.com>
2026-06-06 21:10:58 -0300

    frontend chore: aba paradas

--- frontend/app/(protected)/(tabs)/_layout.tsx
+++ frontend/app/(protected)/(tabs)/_layout.tsx
@@ -4,35 +4,38 @@ export default function TabsLayout() {
   return (
     <Tabs 
       screenOptions={{
-
         tabBarPosition: "top",
 
         headerShown: false, 
       }}
     >
-      <Tabs.Screen 
-        name="dashboard" 
-        options={{ title: "Dashboard" }} 
+      <Tabs.Screen
+        name="dashboard"
+        options={{ title: "Dashboard" }}
       />
-      <Tabs.Screen 
-        name="home" 
-        options={{ title: "Home" }} 
+      <Tabs.Screen
+        name="home"
+        options={{ title: "Home" }}
       />
-      <Tabs.Screen 
-        name="ordens-servico" 
-        options={{ title: "Ordens de Serviço" }} 
+      <Tabs.Screen
+        name="ordens-servico"
+        options={{ title: "Ordens de Servico" }}
       />
-      <Tabs.Screen 
-        name="ativos" 
-        options={{ title: "Ativos" }} 
+      <Tabs.Screen
+        name="ativos"
+        options={{ title: "Ativos" }}
       />
-      <Tabs.Screen 
-        name="alertas" 
-        options={{ title: "Alertas" }} 
+      <Tabs.Screen
+        name="paradas"
+        options={{ title: "Paradas" }}
       />
-      <Tabs.Screen 
-        name="perfil" 
-        options={{ title: "Perfil" }} 
+      <Tabs.Screen
+        name="alertas"
+        options={{ title: "Alertas" }}
+      />
+      <Tabs.Screen
+        name="perfil"
+        options={{ title: "Perfil" }}
       />
     </Tabs>
   );
